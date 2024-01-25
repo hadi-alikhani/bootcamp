05537769b0318d9b67c7fa921c1536de5d7f8f12
hadi-alikhani <76995931+hadi-alikhani@users.noreply.github.com>
2024-01-25 23:11:04 +0330

    Add files via upload

---
+++ Todolist/Todolist/Classes/GroupOfTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Todolist.Enums;
+
+namespace Todolist.Classes
+{
+    internal class GroupOfTask
+    {
+        private List<Tasks> taskToDo = new List<Tasks>()
+        {
+            new Tasks() {CreatedTime = DateTime.Now,DueTime=DateTime.Parse("2024-1-30"),
+                        Title="Meeting",Description="Interview",Priority=PrioritysOptions.high},
+            new Tasks() {CreatedTime = DateTime.Now,DueTime=DateTime.Parse("2024-1-29"),
+                        Title="Shopping",Description="LapTop",Priority=PrioritysOptions.Medium},
+            new Tasks() {CreatedTime = DateTime.Now,DueTime=DateTime.Parse("2024-1-28"),
+                        Title="Meeting",Description="Planning",Priority=PrioritysOptions.low},
+            new Tasks() {CreatedTime = DateTime.Now,DueTime=DateTime.Parse("2024-1-28"),
+                        Title="Bill Paying",Description="CellPhone",Priority=PrioritysOptions.high}
+
+        };
+        public Tasks WhichOneTask()=>taskToDo.OrderBy(i=>i.DueTime).ThenByDescending(i=>i.Priority).First();
+        
+
+    }
+}
---
+++ Todolist/Todolist/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using Todolist.Classes;
+using Todolist.Enums;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Todolist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GroupOfTask todolist = new GroupOfTask();
+           Console.WriteLine(todolist.WhichOneTask());
+
+           
+        }
+    }
+}
