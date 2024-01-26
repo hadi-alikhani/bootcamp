2201ed5d2ce53756c1e6eb9acedbc2df07eb2253
hadi-alikhani <76995931+hadi-alikhani@users.noreply.github.com>
2024-01-26 08:12:44 +0330

    Add files via upload

---
+++ ToDoListUsingJson/ToDoListUsingJson/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.InteropServices;
+using ToDoListUsingJson.classes;
+using ToDoListUsingJson.Entities;
+using ToDoListUsingJson.Enum;
+
+namespace ToDoListUsingJson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // JasonTasks jasonTasks = new JasonTasks();
+            List<Tasks> taskGroup= new List<Tasks>();
+            string input= "yes";
+          while (input.Equals("yes"))
+            {
+                var item = CreateNewTask();
+                taskGroup.Add(item);
+                Console.WriteLine("Are you sure to add Task?");
+                input= Console.ReadLine();
+            }
+          JasonTasks.addTaskToDatabase(taskGroup);
+            Console.WriteLine("Be aware that in the near future you will have to perform the following task");
+            Console.WriteLine(JasonTasks.WhichOneTask(@"E:\Tasks.json"));
+
+        }
+        static Tasks CreateNewTask()
+        {
+            Console.WriteLine("Please insert your Task title:");
+            string title = Console.ReadLine();
+            Console.WriteLine("Please Describe your Task:");
+            string Description = Console.ReadLine();
+            Console.WriteLine("Please insert DueTime of your Task for example 2000-02-01:");
+            DateTime DueTime = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Pleaseinsert priorty of your Task:");
+            PrioritysOptions priority = Console.ReadLine() switch
+            {
+                "high" => PrioritysOptions.high,
+                "low" => PrioritysOptions.low,
+                "Medium" => PrioritysOptions.Medium,
+                _ => throw new Exception("inavalid input")
+            }; ;
+
+            return new Tasks() { Title=title,Description=Description,DueTime=DueTime,CreatedTime=DateTime.Now,Priority
+            =priority};
+        }
+    }
+}
---
+++ ToDoListUsingJson/ToDoListUsingJson/classes/JasonTasks.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ToDoListUsingJson.Entities;
+using ToDoListUsingJson.Enum;
+
+namespace ToDoListUsingJson.classes
+{
+    internal class JasonTasks
+    {
+
+
+       public  static void addTaskToDatabase(List<Tasks>input)
+        {
+            var totaltask = new List<Tasks>();
+            if (File.Exists(@"E:\Tasks.json"))
+            {
+                string contentJsonFile = File.ReadAllText(@"E:\Tasks.json");
+                List<Tasks>? tasks = JsonSerializer.Deserialize<List<Tasks>>(contentJsonFile);
+                 totaltask = tasks.Concat(input).ToList();
+            }
+            string json = JsonSerializer.Serialize(totaltask);
+            File.WriteAllText(@"E:\Tasks.json", json);
+
+        }
+        public static Tasks WhichOneTask(string JasonFilePath)
+        {
+            string contentJsonFile = File.ReadAllText(JasonFilePath);
+            List<Tasks>? tasks = JsonSerializer.Deserialize<List<Tasks>>(contentJsonFile);
+            return tasks.OrderBy(i=>i.DueTime).ThenByDescending(i=>i.Priority).First();
+        }
+    }
+}
